333ff6725495fc7d42efeb974c127e0a92b88f17
Maurus Cuelenaere <mcuelenaere@gmail.com>
2013-12-26 13:31:08 +0100

    Add Blitz templater extension for PHP 5.{3,4,5}

---
+++ Formula/php55-blitz.rb
@@ -0,0 +1,22 @@
+require 'formula'
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php55Blitz < AbstractPhp55Extension
+  init
+  homepage 'http://alexeyrybak.com/blitz/blitz_en.html'
+  url 'http://alexeyrybak.com/blitz/blitz-0.8.6.tar.gz'
+  sha1 '688ceb3579d9da6cd0b84122445ebe7c847c4525'
+
+  def install
+    args = []
+    args << "--prefix=#{prefix}"
+    args << phpconfig
+
+    safe_phpize
+
+    system "./configure", *args
+    system "make"
+    prefix.install "modules/blitz.so"
+    write_config_file unless build.include? "without-config-file"
+  end
+end
