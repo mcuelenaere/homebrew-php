d4e55fcff8d8e6b4edffe35b6180460801b49e94
Maurus Cuelenaere <mcuelenaere@gmail.com>
2016-08-19 11:57:17 +0200

    Add (HEAD-only) GEOS PHP bindings

---
+++ Formula/php70-geos.rb
@@ -0,0 +1,22 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php70Geos < AbstractPhp70Extension
+  init
+  desc "PHP bindings for GEOS"
+  homepage "https://git.osgeo.org/gogs/geos/php-geos"
+  head "https://git.osgeo.org/gogs/geos/php-geos.git"
+
+  depends_on "geos"
+
+  def install
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig,
+                          "--with-geos=#{Formula["geos"].opt_prefix}"
+    system "make"
+    prefix.install "modules/geos.so"
+    write_config_file if build.with? "config-file"
+  end
+end
